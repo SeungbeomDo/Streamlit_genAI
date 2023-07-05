d4f9f317247830826d82b79b6d785cd005a93f77
SeungbeomDo <108214382+SeungbeomDo@users.noreply.github.com>
2023-07-06 08:58:13 +0900

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -19,11 +19,11 @@ import streamlit as st
 from st_pages import Page, show_pages
 
 show_pages(
-    [Page("Codes/dashboard.py", "Main"), #보고서 제목, 목차, 요약 등을 담는 페이지
-     Page("Codes/10_currentstate.py", "배경"), #검토 배경: 생성형 AI의 정의, 산업적 영향에 대한 관심 증가한다는 양적 근거(키워드에 생성형 AI가 들어간 기사 개수 그래프?)
-     Page("Codes/20_casestudy.py", "Case study: 인력 대체 사례"), #기업별 인력 대체 사례: 국가별 산업별로 클릭하면 기업이 나오고, 그 기업의 케이스를 요약해서 전달?
-     Page("Codes/30_forecasting.py", "향후 인력 대체 전망"), #직종별, 국가별 인력 대체와 고용 증가 전망 순위 매긴 barplot, 효율화율 barplot    
-     Page("Codes/40_problems.py", "계열사별 대비 실태") #계열사별로 대비해야 할 사항(인력 수급, 스킬 재교육 등)과 각 사항에 대한 대비 실태?
+    [Page("dashboard.py", "Main"), #보고서 제목, 목차, 요약 등을 담는 페이지
+     Page("10_currentstate.py", "배경"), #검토 배경: 생성형 AI의 정의, 산업적 영향에 대한 관심 증가한다는 양적 근거(키워드에 생성형 AI가 들어간 기사 개수 그래프?)
+     Page("20_casestudy.py", "Case study: 인력 대체 사례"), #기업별 인력 대체 사례: 국가별 산업별로 클릭하면 기업이 나오고, 그 기업의 케이스를 요약해서 전달?
+     Page("30_forecasting.py", "향후 인력 대체 전망"), #직종별, 국가별 인력 대체와 고용 증가 전망 순위 매긴 barplot, 효율화율 barplot    
+     Page("40_problems.py", "계열사별 대비 실태") #계열사별로 대비해야 할 사항(인력 수급, 스킬 재교육 등)과 각 사항에 대한 대비 실태?
     ]
     )
 
